bdcfecbaf6c7730a4ba6fe88bce42b62c32a62a6
js5137 <79146920+js5137@users.noreply.github.com>
2021-05-30 13:17:58 +0900

    Time: 94 ms (6.00%), Space: 39.3 MB (22.33%) - LeetHub

---
+++ two-sum/two-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+		int[] answer = new int[2];
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 1; j < nums.length; j++) {
+				if (nums[i]+nums[j] == target) {
+					if(i != j) {
+						answer[0] = i;
+						answer[1] = j;
+						return answer;
+					}
+				}
+			}
+		}
+
+		return answer;
+	}
+}
